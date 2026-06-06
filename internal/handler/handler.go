493563c2962cbbca49353231b6e519bbd338d738
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-06-06 15:45:36 +0800

    feat: add route newUser getAlluser validation config db connection

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,19 +6,45 @@ import (
 
 	"github.com/gin-gonic/gin"
 	// "gorm.io/driver/postgres"
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
+var validate = validator.New()
+
 func NewUser (db *gorm.DB) gin.HandlerFunc{
 	return func(ctx *gin.Context) {
-		var user *model.Model
+		var user *model.Models
 		ctx.ShouldBindJSON(&user)
+		err := validate.Struct(user)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"status" : 400,
+				"data" : err.Error(),
+			})
+			return
+		}
+		db.Create(user)
 		ctx.JSON(http.StatusOK, gin.H{
 			"data" : user,
 		})
 	}
 }
 
-func GetUser () {
-
+func GetUser (db *gorm.DB) gin.HandlerFunc{
+	return func(ctx *gin.Context) {
+		var user []model.Models
+		db.Find(&user)
+		if len(user) == 0 {
+			ctx.JSON(400, gin.H{
+				"status" : 400,
+				"data" : "Empty user data",
+				})			
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"status" : 200,
+			"data" : user,
+		})
+	}
 }
