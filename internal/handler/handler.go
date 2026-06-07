345d43b5937c135ea5614c77b29c1e67ee1dc322
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-06-07 12:35:37 +0800

    feat: hashingPassword compere password cors

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"midlewerego/internal/model"
+	"midlewerego/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
 	// "gorm.io/driver/postgres"
 	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
@@ -12,39 +14,37 @@ import (
 
 var validate = validator.New()
 
-func NewUser (db *gorm.DB) gin.HandlerFunc{
+func NewUser(db *gorm.DB) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var user *model.Models
 		ctx.ShouldBindJSON(&user)
 		err := validate.Struct(user)
 		if err != nil {
 			ctx.JSON(400, gin.H{
-				"status" : 400,
-				"data" : err.Error(),
+				"status": 400,
+				"data":   err.Error(),
 			})
 			return
 		}
+		hased, err := utils.HashPassword(user.Password)
+		user.Password = hased
+		if err != nil {
+			ctx.JSON(400, gin.H{"status": 400, "data": "failed to hash"})
+		}
 		db.Create(user)
 		ctx.JSON(http.StatusOK, gin.H{
-			"data" : user,
+			"data": hased,
 		})
 	}
 }
 
-func GetUser (db *gorm.DB) gin.HandlerFunc{
+func Login(db *gorm.DB) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var user []model.Models
-		db.Find(&user)
-		if len(user) == 0 {
-			ctx.JSON(400, gin.H{
-				"status" : 400,
-				"data" : "Empty user data",
-				})			
+		var user *model.Models
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(400, gin.H{"status": 400, "data": err.Error()})
 			return
 		}
-		ctx.JSON(http.StatusOK, gin.H{
-			"status" : 200,
-			"data" : user,
-		})
+
 	}
 }
---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateToken(userID uint) (string, error) {
+	if os.Getenv("JWT_SECRET") == "" {
+		panic("missing jwt secret token")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed), err
+}
+
